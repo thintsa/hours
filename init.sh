4d8cb35855d09ed7cf2468a09039318610497840
Timo Hintsa <timo.hintsa@gmail.com>
2015-03-03 22:31:50 +0200

    changed ports

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo adding couple employees
-curl -XPOST http://localhost:3000/employees -d '_id=54f5b64e00c3cbc033777125&name=Timo&birthday=Aug%203,%201978&gender=false&photo=none'
-curl -XPOST http://localhost:3000/hours -d 'employee_id=54f5b64e00c3cbc033777125&date=Mar%203,%202015&category=Coding&hours=3'
+curl -XPOST http://localhost/employees -d '_id=54f5b64e00c3cbc033777125&name=Timo&birthday=Aug%203,%201978&gender=false&photo=none'
+
+curl -XPOST http://localhost/hours -d 'employee_id=54f5b64e00c3cbc033777125&date=Mar%203,%202015&category=Coding&hours=3'
+
+curl -XPOST http://localhost/categories -d 'name=Project work'
+curl -XPOST http://localhost/categories -d 'name=Maintenance'
+curl -XPOST http://localhost/categories -d 'name=Lunch'
+curl -XPOST http://localhost/categories -d 'name=Overtime'
